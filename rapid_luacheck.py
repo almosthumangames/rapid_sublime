50593533a978c8065c4a0f2c3b26445fdc0c7cdd
Taneli Rautio <taneli@almosthumangames.com>
2014-04-04 10:45:34 +0300

    - Luacheck modifications

--- rapid_luacheck.py
+++ rapid_luacheck.py
@@ -10,25 +10,13 @@ from .rapid_parse import RapidSettings
 class RapidLuaCheck():
 	luaCheckLoaded = False
 
-	@staticmethod
-	def loadLuaCheck():
-		rapid_sublime_dir = os.path.dirname(os.path.realpath(__file__))
-		luacheck_path = os.path.abspath(os.path.join(rapid_sublime_dir, "rapid_luacheck.lua"))
-		luacheck_path = luacheck_path.replace('\\', '/')
-		luafile = open(luacheck_path, 'r').read()
-		RapidConnectionThread.checkConnection()
-		line_contents = "@rapid_luacheck.lua:1\n" + luafile + "\000"
-		RapidConnectionThread.instance.sendString(line_contents)
-		RapidLuaCheck.luaCheckLoaded = True
-
 class RapidLuaCheckCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		if self.view.file_name():
-			RapidLuaCheck.loadLuaCheck()
 			current_filename = self.view.file_name().replace("\\", "/")
 			if current_filename.endswith(".lua"):
 				RapidConnectionThread.checkConnection()
-				line_contents = "@rapid_luacheck.py:1\n analyzeFile(\""+ current_filename +"\")\000"
+				line_contents = "@rapid_luacheck.py:1\n require(\"luacheck\")(\""+ current_filename +"\")\000"
 				RapidConnectionThread.instance.sendString(line_contents)
 
 				#open analyze_result.lua in new window
@@ -39,13 +27,11 @@ class RapidLuaCheckCommand(sublime_plugin.TextCommand):
 
 class RapidLuaCheckAllCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		RapidLuaCheck.loadLuaCheck()
-
 		settings = RapidSettings().getSettings()
 		excluded = settings["ExcludedStaticAnalysisFolders"]
 		
 		if RapidFunctionStorage.luaFiles:
-			files = ""
+			files = "{"
 			for path in RapidFunctionStorage.luaFiles:
 				path = path.replace("\\", "/")
 				addToPath = True
@@ -57,11 +43,17 @@ class RapidLuaCheckAllCommand(sublime_plugin.TextCommand):
 						break
 
 				if addToPath == True:
-					files += path + "|"
+					files += "\"" + path + "\","
+
+			files = files[:-1]
+			files += "}"
+
+			print(files)
 
 			RapidOutputView.printMessage("Performing static analysis for all lua files in project...")
 			RapidConnectionThread.checkConnection()
-			line_contents = "@rapid_luacheck.py:1\n analyzeFiles(\""+ files +"\")\000"
+			line_contents = "@rapid_luacheck.py:1\n require(\"luacheck\")"+ files +"\000"
+		
 			RapidConnectionThread.instance.sendString(line_contents)
 
 			#open analyze_result.lua in new window
